e649ba6d7b1f409c0eb25d2da33a28969cda885f
liutianxiang <31911402+Niuniugougou@users.noreply.github.com>
2020-10-26 16:06:41 +0800

    打包。

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ const resolve = dir => require('path').join(__dirname, dir)
 process.env.VUE_APP_VERSION = require('./package.json').version
 process.env.VUE_APP_BUILD_TIME = require('dayjs')().format('YYYY-M-D HH:mm:ss')
 
-// 基础路径 注意发布之前要先修改这里
+// 基础路径 注意发布之前要先修改这里为./
 let publicPath = '/'
 
 module.exports = {
